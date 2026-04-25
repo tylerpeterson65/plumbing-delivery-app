da8299f4d22e1fa2dfb36bb396e48cc9734de445
tylerpeterson65 <tylerpeterson65@yahoo.com>
2026-04-25 07:57:28 -0400

    Create src/App.tsx with main application component

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <h1>Welcome to the Plumbing Delivery App</h1>
+        </div>
+    );
+};
+
+export default App;
